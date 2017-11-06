bbae3bae876992ec7608265fb058a8c503b78f0d
erinacafe <erinacafe@gmail.com>
2017-11-05 20:08:46 -0500

    GuestSearch page 11/5

---
+++ code/php/guestSearch.php
@@ -0,0 +1,107 @@
+<html>
+    
+<title>ACME Search</title>
+
+<?php
+//    $db_connection = pg_connect("host=10.10.7.169 dbname='Corporate_Directory' user=postgres password=password");
+//    
+//if (!$db_connection) {
+//    die("Connection failed: ");
+//
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/Code/CSS/ProfilePage.css">
+    <link rel="stylesheet" type="text/css" href="/Code/CSS/StyleSheet.css">
+    <link rel="stylesheet" type="text/css" href="/Code/CSS/AboutPage.css">
+    <script type="text/javascript" src="/Code/js/ProfilePage.js"></script> 
+    <script type="text/javascript" src="/Code/js/Script.js"></script> <!--Links Script.js to this page-->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Makes Page Responsive -->
+</head>
+
+<body>
+    
+    <!-- Open Hamburger Menu -->
+    <header class="topnav" role="banner">
+        
+        <nav class="topnav container" role="navigation">
+             
+             
+             <!-- Open Hamburger Menu -->
+            <div id="main">
+        	    <!--<span style="font-size:30px;cursor:pointer;" align=left onclick="openNav()">&#9776;</span>-->
+        	    <img src="/Assets/ACMElogo.png" alt="ACMElogo" style="width:100px;height:83px"></img>
+            </div>
+            
+            
+            <div id="myTopnav" class="topnav-list">
+                <a href="/Code/php/guestSearch.php">Home
+                    <span class="fa fa-address-card"></span>
+                </a>
+                <a href="/Code/php/AboutPage.php">About
+                    <span class="fa fa-question-circle"></span>
+                </a>
+                <form class="topnav-list-search">
+                    <input type="text" id="search" name="search" placeholder="&#128269; Search...">
+                </form>               
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+                <a href="/Code/php/SSO.php">Login
+                    <span class="fa fa-space-shuttle"></span>
+                </a>
+                <!--<form class="topnav-list-search">
+                    <input type="text" id="search" name="search" placeholder="Search...">
+                </form>-->
+            
+            </div>
+            <hr id="topHR" noshade>
+            <hr id="topHR" noshade>
+            
+            
+        </nav>
+
+    </header>
+    
+    <!-- Search form -->
+    <div id="main" class="main" align="center" style="margin-top:10%;">
+        
+        <strong><h1 style="margin-top: 5%; color: black;">Welcome to the ACME employee database!</h1></strong><br>
+        <h3 style="margin-top: -1%; font-size: 15px;">Search the directory to find ACME Employees!</h3><br>
+        <form method="post" action="/Code/php/SearchResults.php" id="searchform" style="margin-top: 7%;">
+                <input id="searchbar" class="search" type="text" placeholder="&#128269; Search employees by name or location" name="search">
+                <button class="short" type="submit">
+                    <span class="fa fa-search"></span> Search
+                </button>
+        </form>
+       
+    </div>
+    
+    <!-- Footer -->
+        <div class="footer">
+            <div class="footer-contact" style="text-decoration: none;">
+                <h4>Contact</h4>
+                <a href="">contact@acme.com</a>
+            </div>
+        </div>
+    
+</body>
+
+</html>
+
+<style>
+    .search {
+        padding: 20px;
+    }
+    
+    .short {
+        padding: 20px;
+        width: 140px;
+        cursor: pointer;
+    }
+    
+    .main {
+        margin-bottom: 13%;
+    }
+</style>
