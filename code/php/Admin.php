bf35cd27112824a580c033377b20a374752666d8
myxanders <Mitchell.Xanders1@marist.edu>
2017-11-09 16:20:31 -0500

    Starting work on admin page. Won't get around to SQL until during my night class.

---
+++ code/php/Admin.php
@@ -0,0 +1,96 @@
+<html>
+
+    <title>Administrative Duties</title>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../CSS/StyleSheet.css">
+        <link rel="stylesheet" type="text/css" href="../CSS/ProfilePage.css">
+        <script type="text/javascript" src="../js/ProfilePage.js"></script> 
+        <script type="text/javascript" src="../js/Script.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Makes Page Responsive -->
+    </head>
+    
+    
+    <body>
+    <!-- Navbar -->
+        
+        <header class="topnav" role="banner">
+            
+            <nav class="topnav container" role="navigation">
+    
+                
+                <div id="main">
+            	    <!--<span style="font-size:30px;cursor:pointer;" align=left onclick="openNav()">&#9776;</span>-->
+            	    <img src="../../assets/ACMElogo.png" alt="ACMElogo" style="width:100px;height:83px"></img>
+                </div>
+                
+                
+                <div id="myTopnav" class="topnav-list">
+                    <a href="Search.php">Home
+                        <span class="fa fa-home"></span>
+                    </a>
+                    <a href="profile.php">Profile
+                        <span class="fa fa-address-card"></span>
+                    </a>
+                    <a href="Apps.php">Apps
+                        <span class="fa fa-tasks"></span>
+                    </a>
+                    <a href="SSO.php">Log Out
+                        <span class="fa fa-space-shuttle"></span>
+                    </a>
+                    <!--<form class="topnav-list-search">
+                        <input type="text" id="search" name="search" placeholder="Search...">
+                    </form>-->
+                
+                </div>
+                <hr id="topHR" noshade>
+                <hr id="topHR" noshade>
+                
+                
+            </nav>
+    
+        </header>
+        <br>
+        
+        <div id="mainContent" align="center" style="margin-top:10%;">
+            
+            <h1>Administrative Duties</h1>
+            <h3>You have the following pending requests for Application Access:</h3>
+            
+        </div>
+        <div id="pendingRequests">
+             <?php
+                echo "You have no current requests for application access.";   
+             ?>
+        </div>
+ 
+      
+        <!-- Footer -->
+        <div class="footer">
+            <div class="footer-contact">
+                <h4>Contact</h4>
+                <a href="">contact@acme.com</a>
+            </div>
+            <div class="footer-apps">
+                <h4>Applications</h4>
+                <a href="/Code/php/AccessDenied.php">Salesforce.com</a><br>
+                <a href="/Code/php/AccessDenied.php">Artifactory</a><br>
+                <a href="/Code/php/AccessDenied.php">Teamcity</a><br>
+                <a href="/Code/php/AccessDenied.php">Outlook</a><br>
+                <a href="/Code/php/AccessDenied.php">Skype for Business</a><br>
+                <a href="/Code/php/AccessDenied.php">Assest Management</a>
+            </div>
+        </div>
+        
+    </body>
+
+</html>
+
+<style>
+    #pendingRequests {
+        margin-top: 3%;
+        text-align: center;
+    }
+</style>
