987590c8aa79440a1c74fdfffcaad802901d5272
AlvaR1 <reynaldo.alvarez1896@gmail.com>
2017-11-09 16:57:23 -0500

    Added a Human Resource page; just has a human resource title, a tab in the nav, and two forms to add and remove employee, but they are blank for now;

---
+++ code/php/HR.php
@@ -0,0 +1,105 @@
+<html>
+
+    <title>Human Resources</title>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../CSS/StyleSheet.css">
+        <link rel="stylesheet" type="text/css" href="../CSS/ProfilePage.css">
+        <link rel="stylesheet" type="text/css" href="../CSS/HR.css">
+        <script type="text/javascript" src="../js/ProfilePage.js"></script> 
+        <script type="text/javascript" src="../js/Script.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Makes Page Responsive -->
+    </head>
+    
+    
+    <body>
+    <!-- Navbar -->
+        
+        <header class="topnav" role="banner">
+            
+            <nav class="topnav container" role="navigation">
+    
+                
+                <div id="main">
+            	    <!--<span style="font-size:30px;cursor:pointer;" align=left onclick="openNav()">&#9776;</span>-->
+            	    <img src="../../assets/ACMElogo.png" alt="ACMElogo" style="width:100px;height:83px"></img>
+                </div>
+                
+                
+                <div id="myTopnav" class="topnav-list">
+                    <a href="Search.php">Home
+                        <span class="fa fa-home"></span>
+                    </a>
+                    <a href="profile.php">Profile
+                        <span class="fa fa-address-card"></span>
+                    </a>
+                    <a href="Apps.php">Apps
+                        <span class="fa fa-tasks"></span>
+                    </a>
+                    <a href="HR.php">Human Resources
+                        <span class="fa fa-address-card"></span>
+                    </a>
+                    <a href="SSO.php">Log Out
+                        <span class="fa fa-space-shuttle"></span>
+                    </a>
+                    <!--<form class="topnav-list-search">
+                        <input type="text" id="search" name="search" placeholder="Search...">
+                    </form>-->
+                
+                </div>
+                <hr id="topHR" noshade>
+                <hr id="topHR" noshade>
+                
+                
+            </nav>
+    
+        </header>
+        <br>
+        
+        <div class="title">
+            
+            <h1>Human Resources</h1>
+            
+            
+        </div>
+        
+        <div class="main">
+            <div class="employeeForms">
+
+                <form method="post" action="HR.php" id="addEmployee">
+                    
+                </form>
+
+                <form method="post" action="HR.php" id="removeEmployee">
+                    
+                </form>
+            
+            </div>    
+        </div>
+             
+ 
+      
+        <!-- Footer -->
+        <div class="footer">
+            <div class="footer-contact">
+                <h4>Contact</h4>
+                <a href="">contact@acme.com</a>
+            </div>
+            <div class="footer-apps">
+                <h4>Applications</h4>
+                <a href="/Code/php/AccessDenied.php">Salesforce.com</a><br>
+                <a href="/Code/php/AccessDenied.php">Artifactory</a><br>
+                <a href="/Code/php/AccessDenied.php">Teamcity</a><br>
+                <a href="/Code/php/AccessDenied.php">Outlook</a><br>
+                <a href="/Code/php/AccessDenied.php">Skype for Business</a><br>
+                <a href="/Code/php/AccessDenied.php">Assest Management</a>
+            </div>
+        </div>
+        
+    </body>
+
+</html>
+
+
