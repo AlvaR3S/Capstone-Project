06e762217a2d021637c2c9954417634269266447
erinacafe <erinacafe@gmail.com>
2017-11-16 23:47:00 -0500

    No Navbars for the redirect pages of HR 11/16

--- code/php/HR-remove.php
+++ code/php/HR-remove.php
@@ -1,5 +1,4 @@
 <?php
-
  define('DB_SERVER', 'localhost');
  define('DB_USERNAME', 'root');
  define('DB_PASSWORD', '');
@@ -48,48 +47,7 @@ mysqli_close($link);
         <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Makes Page Responsive -->
     </head>
     <body>
-    <!-- Navbar -->
-        
-        <header class="topnav" role="banner">
-            
-            <nav class="topnav container" role="navigation">
-    
-                
-                <div id="main">
-                    <!--<span style="font-size:30px;cursor:pointer;" align=left onclick="openNav()">&#9776;</span>-->
-                    <img src="../../assets/ACMElogo.png" alt="ACMElogo" style="width:100px;height:83px"></img>
-                </div>
-                
-                
-                <div id="myTopnav" class="topnav-list">
-                    <a href="Search.php">Home
-                        <span class="fa fa-home"></span>
-                    </a>
-                    <a href="profile.php">Profile
-                        <span class="fa fa-address-card"></span>
-                    </a>
-                    <a href="Apps.php">Apps
-                        <span class="fa fa-tasks"></span>
-                    </a>
-                    <a href="HR.php" id="HR">Human Resources
-                        <span id="HR" class="fa fa-address-card"></span>
-                    </a>
-                    <a href="logout.php">Log Out
-                        <span class="fa fa-space-shuttle"></span>
-                    </a>
-                    <!--<form class="topnav-list-search">
-                        <input type="text" id="search" name="search" placeholder="Search...">
-                    </form>-->
-                
-                </div>
-                <hr id="topHR" noshade>
-                <hr id="topHR" noshade>
-                
-                
-            </nav>
-    
-        </header>
-        <br>
+
         
         <div id="mainContent" align="center" style="margin-top:10%;">
             
