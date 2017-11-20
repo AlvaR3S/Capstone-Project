2f91f907aaab24139609be911c08eab43af4acbe
KaiManners <somethingweird102@gmail.com>
2017-11-20 00:31:16 -0500

    created an upload page
    the page uploads pictures to a directory called uploads. will find a way to refer to pictures in uploads to an employee in DB

--- code/php/profile.php
+++ code/php/profile.php
@@ -83,6 +83,7 @@
             <h6 class="locationInfo">Washington, DC</h6>
             <h6 class="departmentInfo"><?php echo '' . $login_title;?></h6>
             <img class="picInfo" src="../../assets/snoopy.jpg" alt="Snoopy">
+			
 			<!-- profile pic code -->
 			<form action="upload.php" method="post" enctype="multipart/form-data">
 				Select image to upload:
