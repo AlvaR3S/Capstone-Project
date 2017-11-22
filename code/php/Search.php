98b10eeed001d6cfa805a7b658c43f19f98dea4f
AlvaR1 <reynaldo.alvarez1896@gmail.com>
2017-11-21 22:25:48 -0500

    Made Search more responsive;

--- code/php/Search.php
+++ code/php/Search.php
@@ -32,7 +32,7 @@ include("nav_check.php");
 	
     <!-- Search form -->
 	
-    <div id="main" class="main" align="center" style="margin-top:10%;">
+    <div id="main" class="main" align="center" style="margin-top:130px;">
         
         <strong><h1 style="margin-top: 5%; color: black;">Welcome to the ACME employee database!</h1></strong><br>
         <h3 style="margin-top: -1%; font-size: 15px;">Search the directory to find ACME Employees!</h3><br>
