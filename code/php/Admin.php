e8456c8828ba216183408510982e5859b340d32d
myxanders <Mitchell.Xanders1@marist.edu>
2017-11-28 20:51:02 -0500

    made some style changes to the admin page. if you dont like I can take it out easy

--- code/php/Admin.php
+++ code/php/Admin.php
@@ -6,7 +6,7 @@ include('admin_check.php');
 
 <html>
 
-    <title>My Applications</title>
+    <title>ACME Admin</title>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="../CSS/StyleSheet.css">
@@ -23,19 +23,39 @@ include('admin_check.php');
     <body>
 
     
-        <div class="adminHead" align=center style="font-size: 45px; margin-top:150px;">Admin Control Panel</div>
+        <div class="adminHead" align=center style="font-size: 45px; margin-top:150px; color:white;">Admin Control Panel</div>
         <div class="containerApps" align=center>
             <h1> Choose an Action to Perform </h1>
             <hr style="border-top:1px solid darkred;">
-                <button class="leftButton" onclick="window.location.href='Manager.php'">
+                <!--button class="leftButton" onclick="window.location.href='Manager.php'">
                     Handle Access Requests
                 </button>
 
                 <button class="rightButton" onclick="window.location.href='HR.php'">
                     Add/Remove Employee
-                </button>
+                </button-->
+                <div class="leftBtn">
+                    <img class="leftBG" src="../../assets/Background/accesspic.jpg" style="border-radius: 20px">
+                    <div class="overlayLeft">
+                        <div class="contentLeft">
+                            <br>Handle Access Requests<br><br><a onclick="window.location.href='Manager.php'"">Go</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rightBtn">
+                    <img class="rightBG" src="../../assets/Background/handshake.jpg" style="border-radius: 20px">
+                    <div class="overlay">
+                        <div class="content">
+                            <br>Add/Remove Employee<br><br><a onclick="window.location.href='HR.php'"">Go</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clear"></div>
 
         </div>
+
         <!-- Footer -->
         <div class="footer">
             <div class="footer-contact">
@@ -59,8 +79,133 @@ include('admin_check.php');
 <style>
 
     h1 {
-        font-size: 25px;
+        font-size: 30px;
     }
+
+    .clear {
+        clear: both;
+    }
+
+    .rightBtn {
+        width:300px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        margin-right: 75px;
+        position: relative;
+        border-radius: 12px;
+        float: right;
+    }
+
+    .rightBG {
+        width: 285px;
+        height: 300px;
+        opacity: 1;
+        transition: .5s ease;
+        border: 1.5px solid #f44336;
+    }
+
+    .content {
+        position: absolute;
+        top: 45%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 24px;
+        color: black;
+        overflow: hidden;
+    }
+
+    .content a {
+        font-size: 18px;
+        color: white;
+        width: 75px;
+        display: block;
+        background-color: #f44336;
+        text-align: center;
+        padding: 10px;
+        cursor: pointer;
+        border-radius: 12px;
+
+    }
+
+    .overlay {
+        opacity: 0;
+    }
+
+    .rightBtn:hover .rightBG{
+        opacity: .2;
+    }
+
+    .rightBtn:hover .overlay {
+        opacity: 1;
+    }
+
+    .leftBtn {
+        width:300px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        margin-left: 75px;
+        position: relative;
+        border-radius: 12px;
+        float: left;
+    }
+
+    .leftBG {
+        width: 285px;
+        height: 300px;
+        opacity: 1;
+        transition: .5s ease;
+        border: 1.5px solid #f44336;
+    }
+
+    .contentLeft {
+        position: absolute;
+        top: 45%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 24px;
+        color: black;
+        overflow: hidden;
+    }
+
+    .contentLeft a {
+        font-size: 18px;
+        color: white;
+        width: 75px;
+        display: block;
+        background-color: #f44336;
+        text-align: center;
+        padding: 10px;
+        cursor: pointer;
+        border-radius: 12px;
+
+    }
+
+    .overlayLeft {
+        opacity: 0;
+    }
+
+    .leftBtn:hover .leftBG{
+        opacity: .2;
+    }
+
+    .leftBtn:hover .overlayLeft {
+        opacity: 1;
+    }
+
+    button.rightButton {
+        background-image: url("../../assets/Background/handshake.jpg");
+        background-size: 600px 400px;
+        background-position: 70% 50%;
+    }
+
+    .leftButton:hover {
+        cursor: pointer;
+    }  
+    
+    .rightButton:hover {
+        cursor: pointer;
+    }  
+
     .app {
         width: 185px;
         height: 185px;
