92651831c98930bfb6777c7f25549ff81986d1a2
erinacafe <erinacafe@gmail.com>
2017-11-30 12:18:46 -0500

    Edits on formatting and profile picture display (profile edit) 11/30

--- code/php/profile_edit.php
+++ code/php/profile_edit.php
@@ -1,6 +1,11 @@
 <?php
    include('session.php');
    include('nav_check.php');
+   
+   $sqlPicture = "select picture from employee where username = '" . $login_session . "'";   
+   $picture = mysqli_query($db,$sqlPicture);   
+   $rowPicture = mysqli_fetch_assoc($picture);
+   $login_picture = $rowPicture['picture'];
 ?>
 <html>
 
@@ -29,7 +34,7 @@
 			<h3 class="personalInfo">Location:</h3>
 			<h5 class="personalInfo"><input style="width: 70%;" type="text" pattern="[A-Za-z]{1,15}" name="location" placeholder="Enter Location"></h5>
 
-			<img class="picInfo" src="uploads/<?php echo $login_picture;?>">
+			<img class="picInfo" style="margin-top: -160px; margin-bottom: -150px;" src="uploads/<?php echo $login_picture;?>">
 			  
 			<h3 class="personalInfo">Profile Picture:</h3>
 			<input class="personalInfo" type="file" name="photo" placeholder="Select image to upload:">
