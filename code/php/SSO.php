eaf54ccfcd16c7179824db9aecb4fc80af6bdff6
KaiManners <somethingweird102@gmail.com>
2017-11-29 20:44:38 -0500

    shitty profile 90 day check

--- code/php/SSO.php
+++ code/php/SSO.php
@@ -20,8 +20,31 @@
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
+				
+				/**
+				//password set date 
+				$pwdset = "SELECT pwdset FROM login WHERE username = '$myusername'";
+				$pwdsettotime = strtotime($pwdset);
+				$pwdsettodate = date("Y/m/d", $pwdsettotime);
+				//current date
+				$currentdate = date("Y/m/d");
+				$NinetyDayCheck = "SELECT DATEDIFF(day, '$currentdate', '$pwdsettodate'";
+				if($NinetyDayCheck < 90){
+				**/
     
      if($count > 0) {
+				//password set date 
+				$pwdset = "SELECT pwdset FROM login WHERE username = '$myusername'";
+				
+				$pwdsettotime = strtotime($pwdset);
+				echo $pwdsettotime . "<br>";
+				$pwdsettodate = date("Y/m/d", $pwdsettotime);
+				echo $pwdsettodate . "<br>";
+				//current date
+				$currentdate = date("Y/m/d");
+				echo $currentdate . "<br>";
+				$NinetyDayCheck = "SELECT DATEDIFF(day, '$currentdate', '$pwdsettodate')";
+				echo $NinetyDayCheck;
 		 $row = mysqli_fetch_array($result);
 		 $password_hash = $row['pwd'];
 			if(password_verify($mypassword, $password_hash)){
