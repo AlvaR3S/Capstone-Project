e45010dc35cf08a64249dbc0002a0cd6d18c502f
Reynaldo Alvarez <reynaldo.alvarez@me.com>
2017-12-07 02:41:51 -0500

    Tried something else for JSON;

--- code/php/JSON_API.php
+++ code/php/JSON_API.php
@@ -1,24 +1,60 @@
+
+
+
+
+
 <?php
 	
-	if($_SERVER['REQUEST_METHOD'] == "GET") {
-		header("Content-type: application/json");
+	
+	// //fetch.php
+		if(isset($_POST["id"]))
+		{
+		 $connect = mysqli_connect("localhost", "root", "", "corporate_directory");
+		 $query = "SELECT * FROM employee WHERE id = '".$_POST["eid"]."'";
+		 $result = mysqli_query($connect, $query);
+		 while($row = mysqli_fetch_array($result))
+		 {
+		  $data["username"] = $row["username"];
+		  $data["firstname"] = $row["firstname"];
+		  $data["streetAddress"] = $row["streetAddress"];
+		  $data["email"] = $row["email"];
+		  $data["dob"] = $row["dob"];
+		 }
 
-		$connect = mysqli_connect("localhost", "root", "", "corporate_directory");
-
-		if(!$connect) {
-			die('Could not connect: '.mysqli_error());
-		} else {
-			echo 'JSON.API';
-		}
-
-		$result = mysqli_query($connect, "SELECT * FROM employee");
-
-		while($row = mysqli_fetch_assoc($result)) {
-			$output[] = $row;
-		}
-
-		print(json_encode($output, JSON_PRETTY_PRINT));
-
-		mysqli_close($connect);
+	 	echo json_encode($data);
 	}
+
+
+
+
+
+
+
+
+
+
+		// header("Content-type: application/json");
+
+		// $connect = mysqli_connect("localhost", "root", "", "corporate_directory");
+
+		// if(!$connect) {
+		// 	die('Could not connect: '.mysqli_error());
+		// } else {
+		// 	echo 'JSON.API';
+		// }
+
+		// $result = mysqli_query($connect, "SELECT * FROM employee");
+
+		// while($row = mysqli_fetch_assoc($result)) {
+		// 	$output[] = $row;
+		// }
+
+		// print(json_encode($output, JSON_PRETTY_PRINT));
+
+		// mysqli_close($connect);
+
+	
 ?>
+
+
+
