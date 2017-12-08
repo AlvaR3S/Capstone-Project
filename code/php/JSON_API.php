28461309df09ee0b2341486c9130f3bb7b96e044
Reynaldo Alvarez <reynaldo.alvarez@me.com>
2017-12-07 19:44:01 -0500

    JSON FINISHING very close;

--- code/php/JSON_API.php
+++ code/php/JSON_API.php
@@ -1,60 +1,24 @@
+<html>
+	<head>
+		<title>JSON Start Page</title>
+	</head>
+	<body>
+		 <form method="GET" action="JSON.php?name" id="search">
+                <input name="name" class="search" type="text" placeholder="search">
+               
+        </form>
 
 
 
 
+	</body>	
 
-<?php
-	
-	
-	// //fetch.php
-		if(isset($_POST["id"]))
-		{
-		 $connect = mysqli_connect("localhost", "root", "", "corporate_directory");
-		 $query = "SELECT * FROM employee WHERE id = '".$_POST["eid"]."'";
-		 $result = mysqli_query($connect, $query);
-		 while($row = mysqli_fetch_array($result))
-		 {
-		  $data["username"] = $row["username"];
-		  $data["firstname"] = $row["firstname"];
-		  $data["streetAddress"] = $row["streetAddress"];
-		  $data["email"] = $row["email"];
-		  $data["dob"] = $row["dob"];
-		 }
 
-	 	echo json_encode($data);
-	}
+</html>
 
 
 
 
-
-
-
-
-
-
-		// header("Content-type: application/json");
-
-		// $connect = mysqli_connect("localhost", "root", "", "corporate_directory");
-
-		// if(!$connect) {
-		// 	die('Could not connect: '.mysqli_error());
-		// } else {
-		// 	echo 'JSON.API';
-		// }
-
-		// $result = mysqli_query($connect, "SELECT * FROM employee");
-
-		// while($row = mysqli_fetch_assoc($result)) {
-		// 	$output[] = $row;
-		// }
-
-		// print(json_encode($output, JSON_PRETTY_PRINT));
-
-		// mysqli_close($connect);
-
-	
-?>
 
 
 
