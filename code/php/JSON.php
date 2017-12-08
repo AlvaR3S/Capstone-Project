a324f32b6c88dcf0258d3549b2e947adb1b086d5
Reynaldo Alvarez <reynaldo.alvarez@me.com>
2017-12-07 20:04:00 -0500

    CLOSE but not close...

--- code/php/JSON.php
+++ code/php/JSON.php
@@ -29,23 +29,22 @@
 		if($_SERVER['REQUEST_METHOD'] = 'GET') {
 		
 			if(isset($_GET["name"])) {
+
 				 $connect = mysqli_connect("localhost", "root", "", "corporate_directory");
+				 
 				 $query = "SELECT firstname FROM employee";
+
 				 $result = mysqli_query($connect, $query);
-				 while($row = mysqli_fetch_array($result))
-				 {
-				  $data["firstname"] = $row["firstname"];
-				 }
-				 header('Content-Type: application/json');
-			 	print(json_encode($data, JSON_PRETTY_PRINT));
+				 $row = mysqli_fetch_array($result);
+				 
+				
+				 
+				 	header('Content-Type: application/json');
+			 		print(json_encode($row, JSON_PRETTY_PRINT));
 			 	
 			} else {
 				echo 'not found';
 			}
-
-
-
-			
 		}
 
 	
